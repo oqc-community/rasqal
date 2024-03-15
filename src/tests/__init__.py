27859f69121aeaca391c96bf81cfc37abbd391fb
John Dumbell <euphoriceulogy@gmail.com>
2024-03-15 13:57:51 +0000

    Use default log file name for tests

--- src/tests/__init__.py
+++ src/tests/__init__.py
@@ -1,7 +1,7 @@
 import os
 import pathlib
 
-from rasqal.utils import initialize_logger
+from rasqal.utils import initialize_logger, DEFAULT_LOG_FILE
 
 # Automatically initialize file logging for tests.
-initialize_logger(os.path.join(pathlib.Path(__file__).parent.resolve(), "rasqal.txt"))
+initialize_logger(os.path.join(pathlib.Path(__file__).parent.resolve(), DEFAULT_LOG_FILE))
