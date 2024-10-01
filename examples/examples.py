0d1263d734115e2f7c71b0659e139f6478352548
John Dumbell <euphoriceulogy@gmail.com>
2024-10-01 14:30:59 +0100

    Update project file
    And some minor updates to examples.

--- examples/examples.py
+++ examples/examples.py
@@ -103,35 +103,32 @@ def basic_loops():
 
     @EntryPoint()
     operation Run(numSegments: Int) : Int {
-      use (a, b, c) = (Qubit(), Qubit(), Qubit());
-      use reg = Qubit[3];
-      ApplyToEach(H, reg);
-
       mutable incrementing_result = 0;
       for index in 0..numSegments {
-        ApplyToEach(H, reg);
+        use reg = Qubit[3] {
+          ApplyToEach(H, reg);
 
-        // Would use modulo here but need to add it to allowed instructions.
-        let is_even = index == 2 || index == 4 || index == 6 || index == 8 || index == 10;
-        if is_even {
-          H(reg[0]);
-          H(reg[2]);
+          // Would use modulo here but need to add it to allowed instructions.
+          let is_even = index == 2 || index == 4 || index == 6 || index == 8 || index == 10;
+          if is_even {
+            H(reg[0]);
+            H(reg[2]);
+          }
+
+          let result = ResultArrayAsBoolArray(MultiM(reg));
+          mutable appending = 0;
+          if result[0] { set appending = appending + 1; }
+          if result[1] { set appending = appending + 1; }
+          if result[2] { set appending = appending + 1; }
+
+          set incrementing_result = incrementing_result + appending;
         }
-
-        let result = ResultArrayAsBoolArray(MultiM(reg));
-        mutable appending = 0;
-        if result[0] { set appending = appending + 1; }
-        if result[1] { set appending = appending + 1; }
-        if result[2] { set appending = appending + 1; }
-
-        set incrementing_result = incrementing_result + appending;
       }
 
       return incrementing_result * 5;
     }
     """
     print(f"Running basic-loops.")
-    # TODO: Check execution path, something slightly wrong here.
     runner = fetch_runner(4)
     results = runner.run_ll(read_qir_file("basic-loops.ll"), [5])
     print(f"Returned {results}")
